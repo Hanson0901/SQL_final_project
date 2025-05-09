8e2cb9a9edf2c6f215a96db164202a1ac02be63b
Hanson0901 <101704292+Hanson0901@users.noreply.github.com>
2025-05-10 01:15:40 +0800

    update

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def api_games():
 
 if __name__ == '__main__':
     context = (
-        "/etc/letsencrypt/live/cgusqlpj.ddns.net/fullchain.pem",
-        "/etc/letsencrypt/live/cgusqlpj.ddns.net/privkey.pem"
+        "/home/cbes100070/pem/fullchain.pem",
+        "/home/cbes100070/pem/privkey.pem"
     )
     app.run(host='0.0.0.0',debug=True, ssl_context=context)
