0b0c949811449c1a3bd71d08575cea4dcdc1f065
Hanson0901 <101704292+Hanson0901@users.noreply.github.com>
2025-05-10 00:07:21 +0800

    fix markup

--- get_mlb_score.py
+++ get_mlb_score.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
+from markupsafe import Markup
 import json
 import datetime
 import os
