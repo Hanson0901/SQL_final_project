b20d9f350f3efb45615f572ec8e5281584cc409c
WeiChengTW <chang93928@gmail.com>
2025-05-27 21:24:09 +0800

    5001

--- Live_Scores/static/js/BWF.js
+++ Live_Scores/static/js/BWF.js
@@ -14,7 +14,7 @@ let lastMatchCount = 0;
 async function fetchAndRenderBWF() {
   const ul = document.getElementById('bwf-match-list');
   try {
-    const response = await fetch('http://127.0.0.1:5000/app/BWFscore');
+    const response = await fetch('http://127.0.0.1:5001/app/BWFscore');
     const data = await response.json();
 
     // 如果賽事數量不同，重建 li 結構
