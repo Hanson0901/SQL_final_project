be824d4859f9d38bbfa29cc8c7c8c3243ca28d2d
Hanson0901 <101704292+Hanson0901@users.noreply.github.com>
2025-05-27 22:31:53 +0800

    fix nba

--- LineBot/linebotserver.py
+++ LineBot/linebotserver.py
@@ -82,7 +82,8 @@ def handle_message(event):
                                 text="請選擇賽事",
                                 actions=[
                                     MessageAction(label="CPBL", text="CPBL"),
-                                    MessageAction(label="BWF", text="BWF")
+                                    MessageAction(label="BWF", text="BWF"),
+                                    MessageAction(label="NBA", text="NBA")  # 補足三個
                                 ]
                             )
                         ]
