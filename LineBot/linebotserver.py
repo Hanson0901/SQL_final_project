aff36d04755ed062b39bccc819d08969445b7fef
Hanson0901 <101704292+Hanson0901@users.noreply.github.com>
2025-05-29 21:32:32 +0800

    add return

--- LineBot/linebotserver.py
+++ LineBot/linebotserver.py
@@ -310,17 +310,18 @@ def claim_feedback():
     data = request.get_json()
     print("收到訊息")
     print(data)  # 你可以印出收到的內容
-    return "OK"
+    
     #傳送給該uid的使用者
     user_id = data.get('uid')
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.push_message(
-            PushMessageRequest(
-                to=user_id,
-                messages=TextMessage(text="您的意見反映已被認領並進入處理中，感謝您的回饋！")
+        PushMessageRequest(
+            to=user_id,
+            messages=TextMessage(text="您的意見反映已被認領並進入處理中，感謝您的回饋！")
             )
         )
+    return "OK"
 
 
 
