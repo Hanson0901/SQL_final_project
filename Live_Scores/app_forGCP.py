9726d6f2a73e4337ce329853033b6d3261da7bf9
Hanson0901 <101704292+Hanson0901@users.noreply.github.com>
2025-05-30 12:57:50 +0800

    cors

--- Live_Scores/app_forGCP.py
+++ Live_Scores/app_forGCP.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request, render_template
-
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 NBA_SCORE_FILE = "nba_score.json"
 BWF_SCORE_FILE = "bwf_score.json"
 
