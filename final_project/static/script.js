d5d1a63e9131b35fb056be99c54f771cb278a877
hannshushua <hannshushua@gmail.com>
2025-05-31 16:47:19 +0800

    dsadas

--- final_project/static/script.js
+++ final_project/static/script.js
@@ -2700,7 +2700,7 @@ try {
               if (diffMinutes < 15) {
                   btn.classList.add("disabled");
                   btn.addEventListener("click", () => {
-                  alert(`此比賽已過或即將開始，無法預約。\n ${matchObj.name}\n ${dateStr} | ${matchObj.time}<br>
+                  alert(`此比賽已過或即將開始，無法預約。\n ${matchObj.name}\n ${dateStr} | ${matchObj.time}\n
                   詳細比分資訊可去比賽搜尋區查看 !`);
               });
               } else {
